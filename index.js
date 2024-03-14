81b93274c0e54c6086488d61c9185d0a15d6626d
Faizakarimi <38805945+Faizakarimi@users.noreply.github.com>
2024-03-14 13:47:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const convertBtn = document.getElementById('convert-btn')
 const numToConvertEl = document.getElementById('num-to-convert')
 
 
-convertBtn.addEventListener('click', function(){
+convertBtn.addEventListener('click', function () {
     const numToConvert = Number(numToConvertEl.value)
-    resultSec.innerHTML = convertToLength(numToConvert)
-    resultSec.innerHTML += convertToVolume(numToConvert)
-    resultSec.innerHTML += convertToMass(numToConvert)
+    if (numToConvert) {
+        resultSec.innerHTML = convertToLength(numToConvert)
+        resultSec.innerHTML += convertToVolume(numToConvert)
+        resultSec.innerHTML += convertToMass(numToConvert)
+
+    }
+
 
 })
 
@@ -15,32 +19,32 @@ convertBtn.addEventListener('click', function(){
 function convertToLength(length) {
 
     const feetToMeter = (length * 0.3048).toFixed(3)
-    const meterToFeet = (length * 3.28084) .toFixed(3)
-        return `<div class="result-container">
+    const meterToFeet = (length * 3.28084).toFixed(3)
+    return `<div class="result-container">
 <h2 id="result-heading">Length (Meter/Feet)</h2>
 <h3 id="result-text"  class="white-text">${length} meters = ${meterToFeet} feet | ${length} feet = ${feetToMeter} meters</h3>
 </div>`
 
 }
 
-function convertToVolume(volume){
+function convertToVolume(volume) {
 
-    const literToGallen = (volume * 0.264172) .toFixed(3)
-    const gallenToLiter = (volume * 3.78541) . toFixed(3)
+    const literToGallen = (volume * 0.264172).toFixed(3)
+    const gallenToLiter = (volume * 3.78541).toFixed(3)
 
     return `<div class="result-container">
     <h2 id="result-heading">Volume (Liters/Gallons)</h2>
-    <h3 id="result-text" class="white-text">${volume} liters = ${literToGallen} gallons | ${volume } gallons = ${gallenToLiter} liters</h3>
+    <h3 id="result-text" class="white-text">${volume} liters = ${literToGallen} gallons | ${volume} gallons = ${gallenToLiter} liters</h3>
     </div>`
 
 }
 
-function convertToMass(mass){
-    const kgToPound = (mass * 2.20462) .toFixed(3)
-    const poundTokg = (mass * 0.453592) .toFixed(3)
+function convertToMass(mass) {
+    const kgToPound = (mass * 2.20462).toFixed(3)
+    const poundTokg = (mass * 0.453592).toFixed(3)
     return `<div class="result-container">
     <h2 id="result-heading">Mass (Kilograms/Pounds)</h2>
-    <h3 id="result-text"  class="white-text">${mass} kilos = ${kgToPound} pounds | ${mass } pounds = ${poundTokg} kilos</h3>
+    <h3 id="result-text"  class="white-text">${mass} kilos = ${kgToPound} pounds | ${mass} pounds = ${poundTokg} kilos</h3>
     </div>`
 
 
